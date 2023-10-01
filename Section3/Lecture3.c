cd0394f20c2c03db45ea783b817ef523401c9fb9
World Big (BigWorld) <bigworld3863@gmail.com>
2023-10-01 09:07:01 +0000

    first commit 23.10.01 18:06

---
+++ Section3/Lecture3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("dfsdf\n");
+	
+	return 0;
+}
---
+++ src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello, World!\n");
+	
+	return 0;
+}
+//build code 
+//.c file to bin folder
+//mkdir -p /workspace/C_Study/bin/ && gcc /workspace/C_Study/SectionA/LectureB.c -o /workspace/C_Study/bin//LectureB -g -lm
