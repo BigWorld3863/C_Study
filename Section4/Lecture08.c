402bb10be242d35887e935d6b95abdab4f148d81
World Big (BigWorld) <bigworld3863@gmail.com>
2023-10-08 08:21:20 +0000

    23.10.08 17:21

--- Section4/Lecture08.c
+++ Section4/Lecture08.c
@@ -17,18 +17,21 @@ int main()
 	printf("%*i\n", 7, 456);
 	
 	printf("\nPrecision\n");
-	// printf("%.3d\n", 1024);
-	// printf("%.5d\n", 1024);
-	// printf(".3f\n", 123456.1234567);
-	// printf(".3f\n", 123456.1235);
-	// printf("10.3f\n", 123.45678);
-	// printf("\n",);
-	// printf("\n",);
-	// printf("\n",);
+	printf("%.3d\n", 1024);
+	printf("%.5d\n", 1024);
+	printf("%.3f\n", 123456.1234567);
+	printf("%.3f\n", 123456.1235);
+	printf("%10.3f\n", 123.45678);
+	printf("%010.3f\n", 123.45678);
+	printf("%.5s\n", "ABCDEFGHIJKLMN");
+	printf("%.s\n", "ABCDEFGHIJKLMN"); // assumes .0
 	
+	printf("\nLength\n");
+	printf("%hhd %hd %d\n", 257, 257, 257);
+	printf("%d %lld %lld\n", INT_MAX + 1, INT_MAX + 1, 2147483648LL); // difference between x64 & x86 
+
+	// printf("%-010i\n", 1234567);
+	// printf("%0+10.5hi", 256);
 	
-		// tomorrow think about:
-		printf("%-010i\n", 1234567);
-		printf("%0+10.5hi", 256);
 	return 0;
 }
---
+++ Section4/Lecutre10.c
@@ -0,0 +1,15 @@
+// scanf() instruction
+
+#include <stdio.h>
+#include <inttypes.h> // intmax_t
+
+int main()
+{
+	// multiple inputs with blank separators
+	int i;
+	float f;
+	char str[30];
+	
+	
+	return 0;
+}
