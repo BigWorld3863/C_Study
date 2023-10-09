9e415e7e3356aa58e26c95838433c1d18053a828
World Big (BigWorld) <bigworld3863@gmail.com>
2023-10-09 11:52:30 +0000

    23.10.09 20:52

--- Section5/Lecture07.c
+++ Section5/Lecture07.c
@@ -1,22 +1,45 @@
+// % operator
 #include <stdio.h>
 
 int main()
 {
-	int hours = 0, minutes = 0, seconds = 0;
+	// int hours = 0, minutes = 0, seconds = 0;
 	
-	while (seconds >= 0)
-	{
-		printf("Input seconds : ");
-		scanf("%d", &seconds);
+	// while (seconds >= 0)
+	// {
+	// 	printf("Input seconds : ");
+	// 	scanf("%d", &seconds);
 
-		hours = seconds / 3600;
-		seconds %= 3600;
+	// 	hours = seconds / 3600;
+	// 	seconds %= 3600;
 
-		minutes = seconds / 60;
-		seconds %= 60;
+	// 	minutes = seconds / 60;
+	// 	seconds %= 60;
+	// 	if (seconds >= 0)
+	// 	{
+	// 	printf("%d hours, %d minutes, %d seconds\n", hours, minutes, seconds);
+	// 	}
+	// }
 	
-		printf("%d hours, %d minutes, %d seconds\nGood bye\n\n", hours, minutes, seconds);
-	}
+	// printf("Good bye\n");
 	
+	int div, mod;
+	
+	div = 11 / 5; 
+	mod = 11 % 5; 
+	printf("div = %d, mod = %d\n", div, mod);
+
+	div = 11 / -5; 
+	mod = 11 % -5; 
+	printf("div = %d, mod = %d\n", div, mod);
+
+	div = -11 / -5; 
+	mod = -11 % -5;
+	printf("div = %d, mod = %d\n", div, mod);
+
+	div = -11 / 5;
+	mod = -11 % 5;
+	printf("div = %d, mod = %d\n", div, mod);
+
 	return 0;
 }
---
+++ Section5/Lecture08.c
@@ -0,0 +1,27 @@
+// ++, -- operator
+#include <stdio.h>
+
+int main()
+{
+	// int a = 0;
+	// a++;
+	// printf("%d\n", a);
+
+	// ++a;
+	// printf("%d\n", a);
+	
+	// double b = 0;
+	// b++;
+	// printf("%f\n", b);
+	
+	// b++;
+	// printf("%f\n", b);
+	
+	int count = 0;
+	while (count < 10) // ++count or count++
+	{
+		printf("%d\n", count++); // ++count or count++
+		// ++count;
+	}
+	return 0;
+}
