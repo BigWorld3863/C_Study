5cbf3289afd2234a93a931f7d0c134f8dd7402bd
World Big (BigWorld) <bigworld3863@gmail.com>
2023-10-19 14:56:17 +0000

    23.10.19 23:56

--- Section6/Lecture15.c
+++ Section6/Lecture15.c
@@ -1,29 +1,81 @@
 // Nested loops
 #include <stdio.h>
 
+#define NUM_ROWS	5
+#define FIRST_CHAR	'A'
+#define LAST_CHAR	'J'
+
 int main()
-{
-	char first_char = 'A';
-	char last_char = '\0';
-	unsigned repeated_time = 0;
+{	
+	// exercise 1
 	
-	printf("Input last char you want: ");
-	scanf("%c", &last_char);
+	// char first_char = 'A';
+	// char last_char = '\0';
+	// unsigned repeated_time = 0;
 	
-	printf("Enter how many times you want to repeat: ");
-	scanf("%d", &repeated_time);
+	// printf("Input last char you want: ");
+	// scanf("%c", &last_char);
 	
-	for (int i = 0; i < repeated_time; i++)
-	{	
-		while (first_char <= last_char)
-		{
-			printf("%c ", first_char);
-			first_char++;
-		}
+	// printf("Enter how many times you want to repeat: ");
+	// scanf("%d", &repeated_time);
+	
+	// for (int i = 0; i < repeated_time; i++)
+	// {	
+	// 	while (first_char <= last_char)
+	// 	{
+	// 		printf("%c ", first_char);
+	// 		first_char++;
+	// 	}
 		
-		first_char = 'A';
-		printf("\n");
-	}
+	// 	first_char = 'A';
+	// 	printf("\n");
+	// }
+	
+	int r; // row loop
+	int c; // character loop
+	
+	// for (r = 0; r < NUM_ROWS; r++) // outer row loop
+	// {	
+	// 	for (c = FIRST_CHAR; c <= LAST_CHAR; c++) // inner character loop
+	// 		printf("%c ", c);
+		
+	// 	printf("\n");
+	// }
+	
+	// exercise 2
+	
+	// int rows = LAST_CHAR - FIRST_CHAR + 1;
+	// char last_char = FIRST_CHAR;
+	
+	// for (r = 0; r < rows; r++) // outer row loop
+	// {	
+	// 	for (c = FIRST_CHAR; c <= last_char; c++)
+	// 		printf("%c ", c);
+
+	// 	last_char++;
+	// 	printf("\n");
+	// }
+	
+	for (r = 0;)
+	
+	
+	
+	
+	
+	//think another way;
+real last char = k
+fake last char = first char
+for (fake last char <= real last char; fake last char++)
+	
+	for (first char <=fake last char; first char++)
+		printf(fist char)
+	
+	printf("\n")
+	
+	
+	
+	
+	
 	
 	return 0;
 }
