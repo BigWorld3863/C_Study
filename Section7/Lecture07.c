6180085259506c9e1884734aefb02ae66f424da4
World Big (BigWorld) <bigworld3863@gmail.com>
2023-11-27 16:35:25 +0000

    23.11.28 01:35

--- Section7/Lecture07.c
+++ Section7/Lecture07.c
@@ -1,18 +1,27 @@
-// Logical Operators
+// Logical operators
+
 #include <stdio.h>
-#include <ctype.h>	// islower()
+#include <ctype.h> // islower()
 #include <stdbool.h>
-#include <iso646.h>	// and, or, not
+#include <iso646.h>
 
 #define PERIOD '.'
 
 int main()
 {
-	// bool test1 = 3 > 2 || 5 > 6;	// true
-	// bool test2 = 3 > 2 && 5 > 6;	// false
-	// bool test3 = !(5 > 6);			// true , equivalent to 5 <= 6
+	/*
+		Logical operators
+		
+		&& : and
+		|| : or
+		!  : not
+	*/
 	
-	//printf("%d %d %d\n", test1, test2, test3);
+	// bool test1 = 3 > 2 || 5 > 6;
+	// bool test2 = 3 > 2 && 5 > 6;
+	// bool test3 = !(5 > 6);			// true, equivalent to 5 <= 6
+	
+	// printf("%d %d %d\n", test1, test2, test3);
 	
 	// Character counting example
 	
@@ -21,45 +30,75 @@ int main()
 	
 	// while ((ch = getchar()) != PERIOD)
 	// {
+	// 	//TODO: make exceptions	
 	// 	if (ch != '\n' && ch != ' ')
 	// 		count++;
 	// }
-
+	
 	// printf("%d\n", count);
 	
 	/*
-		Precedence
+		Alternatives in iso646.c
 		
-		https://en.cppreference.com/w/c/language/operator_precedence
+		&& : and
+		|| : or
+		!  : not
 	*/
+	
+	// Precedence
 	
 	// int a = 1, b = 2, c = 3, d = 4;
 	
 	// a > b && b > c || b > d;
 	// ((a > b) && (b > c)) || (b > d);
 	
-	/*
-		Short-circuit Evaluaion
+	
+	/* 
+		Short-circuit Evaluation
 		
 		- Logical expressions are evaluated from left to right
-		- && and || are sequence points 
+		- && and || are sequence points
 	*/
-
+	
 	// int temp = (1 + 2) * (3 + 4);
 	
 	// printf("Before : %d\n", temp);
 	
 	// if (temp == 0 && (++temp == 1024))
 	// {
-	// 	// do nothing
+	// 	//do nothing
 	// }
 	
 	// printf("After : %d\n", temp);
 	
-	int x = 1, y = 2;
-	if (x++ > 0 && x + y == 4) // && is sequence point
-		printf("%d %d\n", x, y);
+	// int x = 1, y = 2;
 	
+	// if (x++ > 0 && x + y == 4)
+	// 	printf("%d %d\n", x, y);
+
+	/* Ranges */
+	
+	// for (int i = 0; i < 100; ++i)
+	// 	if (i >= 10 && i <= 20)
+	// 		printf("%d ", i);
+	
+	// printf("\n");
+	
+	// for (int i = 0; i < 100; ++i)
+	// 	if (10 <= i <= 20)		//Note: if ((10 <= i) <= 20)
+	// 		printf("%d ", i);
+	
+	// printf("\n");
+	
+	for (char c = -128; c < 127; c++)
+		if (c >= 'a' && c <= 'z')
+			printf("%c ", c);
+	
+	printf("\n");
+	
+	for (char c = -128; c < 127; c++)
+		if (islower(c))
+			printf("%c ",c);
 	
 	return 0;
 }
---
+++ Section7/Lecture08.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+#define PERIOD '.'
+
+int main()
+{
+	char ch = 0;
+	
+	int cnt_all = 0, cnt_words = 0, cnt_lines = 0;
+	
+	printf("Enter text : \n");
+	
+	while (ch = getchar() != PERIOD)
+	{	
+		if (ch != ' ')
+		
+		// cnt all 
+		// cnt words islower() || isupper()
+		// cnt lines '\n'
+		
+		//cnt_all++;
+		
+	 
+	 
+	}
+	return 0;
+}
