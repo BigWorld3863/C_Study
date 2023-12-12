4940b08f275440d51a84965026054d567c9b09ac
World Big (BigWorld) <bigworld3863@gmail.com>
2023-12-12 16:17:10 +0000

    23.12.13 01:17

---
+++ Section9/Lecture01.c
@@ -0,0 +1,41 @@
+// function
+#include <stdio.h>
+#include <string.h>	//strlen()
+
+#define WIDTH 30
+#define NAME "Sung-Hyun Shin"
+#define ADDRESS "Seoul, Korea"
+
+void print_multiple_chars(char c, int n_stars);
+void print_cantered_str(char str[]);
+
+int main()
+{
+	print_multiple_chars('*', WIDTH);
+	
+	print_cantered_str(NAME);
+	print_cantered_str(ADDRESS);
+	print_cantered_str("I love you");
+	
+	print_multiple_chars('*', WIDTH);
+	
+	return 0;
+}
+
+void print_multiple_chars(char c, int n_stars)
+{
+	for (int i= 0; i < n_stars; i++)
+		printf("%c", c);
+	
+	if (c != ' ')
+		printf("\n");
+}
+
+void print_cantered_str(char str[])
+{
+	int n_blanks = 0;
+	
+	n_blanks = (WIDTH - strlen(str)) / 2;
+	print_multiple_chars(' ', n_blanks);
+	printf("%s\n", str);
+}
