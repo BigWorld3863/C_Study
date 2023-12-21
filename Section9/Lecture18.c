8539f677bbcad135fc81ae572eb01fb1b17dce5a
World Big (BigWorld) <bigworld3863@gmail.com>
2023-12-21 14:12:37 +0000

    23.12.21 23:12 Sec9 completed

---
+++ Section9/Lecture18.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void swap(int *u, int *v)	//call by pointer
+{	
+	printf("%p %p\n", u, v); 
+	
+	int temp = *u;
+	*u = *v;
+	*v = temp;
+}
+
+int main()
+{
+	int a = 123;
+	int b = 456;
+	
+	printf("%p %p\n", &a, &b);
+	
+	swap(&a, &b);
+	
+	printf("%d %d", a, b);
+
+	return 0;
+}
